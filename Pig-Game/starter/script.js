e2132be01245808da584c54cb1b4ce04f2a1cb5f
ahsan70 <ahsan-ali70@outlook.com>
2021-08-02 06:11:18 -0700

    Implement Restarting game functionality

--- Pig-Game/starter/script.js
+++ Pig-Game/starter/script.js
@@ -12,15 +12,25 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 //Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
 
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+init();
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   currentScore = 0;
@@ -34,7 +44,7 @@ btnRoll.addEventListener('click', function () {
   if (playing) {
     // 1.Generating a random dice roll
     const dice = Math.trunc(Math.random() * 6) + 1;
-    console.log(dice);
+
     // 2.Display dice
     diceEl.classList.remove('hidden');
     diceEl.src = `dice-${dice}.png`;
@@ -73,3 +83,5 @@ btnHold.addEventListener('click', function () {
     //Switch to the next player
   }
 });
+
+btnNew.addEventListener('click', init);
