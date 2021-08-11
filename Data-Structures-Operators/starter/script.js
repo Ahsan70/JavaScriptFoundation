e7ae8072e44661a6bd8319b810ac51dfde684f40
Ahsan <ahsan-ali70@outlook.com>
2021-08-11 18:30:12 +0500

    Destructuring Arrays

--- Data-Structures-Operators/starter/script.js
+++ Data-Structures-Operators/starter/script.js
@@ -12,18 +12,12 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
+  order:function(staterIndex,mainIndex){
+    return [this.starterMenu[staterIndex],this.mainMenu[mainIndex]];
+  }
 };
+
+const [starter,mainCourse]=restaurant.order(2,0);
+console.log(starter,mainCourse);
+
+
